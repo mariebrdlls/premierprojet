f7e9cf6d2d02aa308c117a8f10b7a9ec959f7626
mariebrdlls <marie.brdlls@gmail.com>
2023-02-17 11:20:50 +0100

    deuxième version

--- testr.R
+++ testr.R
@@ -1,6 +1,10 @@
 #ceci est un test
 
-#&fonction qui renvoie la somme de deux nombres
+#fonction qui renvoie la somme de deux nombres
 somme <- function(x,y)
-{return(x+y)}
+  {return(x+y)}
 
+#fonction qui renvoie la différence entre deux nombres
+différence <- function(x,y)
+  {return(x-y)}
+  
